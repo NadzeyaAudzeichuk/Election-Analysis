915a30695d13aa874ca467bbed804f098120cebc
21avdzeichuk@gmail.com <44038219+NadzeyaAudzeichuk@users.noreply.github.com>
2022-08-15 13:32:13 -0400

    Added candidate results and winning candidate to election_analysis.txt

--- PyPoll.py
+++ PyPoll.py
@@ -54,33 +54,35 @@ with open(file_to_save, "w") as txt_file:
         f"Total Votes: {total_votes:,}\n"
         f"---------------------------\n")
     print(election_results, end="")
+    # Write election results in election_analysis.txt
     txt_file.write(election_results)
 
 
 # print(candidate_option)
 # print(candidate_votes)
 
-for candidate_name in candidate_option:
-    votes = candidate_votes[candidate_name]
+    for candidate_name in candidate_option:
+        votes = candidate_votes[candidate_name]
 
-    vote_percentage = float(votes)/float(total_votes) * 100
-    #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        vote_percentage = float(votes)/float(total_votes) * 100
+        candidate_results = (
+            f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
+        #Write candidates results in election_analysis.txt
+        txt_file.write(candidate_results)
 
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = candidate_name
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
 
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Votes Count: {winning_count:,}\n"
-    f"winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Votes Count: {winning_count:,}\n"
+        f"winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
 
-#print(winning_candidate_summary)
-
-# with open(file_to_save,"w") as txt_file:
-#     txt_file.write("Counties in the Election\n")
-#     txt_file.write("-------------------------\n")
-#     txt_file.write("Arapahoe\nDenver\nJefferson\n ")
+    print(winning_candidate_summary)
+    #Write winning candidate in election_analysis.txt
+    txt_file.write(winning_candidate_summary)
